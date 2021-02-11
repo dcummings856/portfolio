5378ad56225e5c04e2bd26087c631954270b3062
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-02-11 08:04:13 -0500

    updated some styles and added variables to js file

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const github = document.querySelector('.li-github')
+const resume = document.querySelector('.li-resume')
+const twitter = document.querySelector('.li-twitter')
+const instagram = document.querySelector('.li-instagram')
