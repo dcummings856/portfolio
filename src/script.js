2355db846f73b25a5825212da5eb6421535d097a
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-02-13 11:29:16 -0500

    event listeners and functions

--- src/script.js
+++ src/script.js
@@ -2,3 +2,27 @@ const github = document.querySelector('.li-github')
 const resume = document.querySelector('.li-resume')
 const twitter = document.querySelector('.li-twitter')
 const instagram = document.querySelector('.li-instagram')
+
+github.addEventListener('click', getGithub)
+
+function getGithub () {
+  window.location.href = "https://github.com/dcummings856"
+}
+
+resume.addEventListener('click', getResume)
+
+function getResume () {
+  window.location.href = ""
+}
+
+twitter.addEventListener('click', getTwitter)
+
+function getTwitter () {
+  window.location.href = "https://twitter.com/dcummings856"
+}
+
+instagram.addEventListener('click', getInstagram)
+
+function getInstagram () {
+  window.location.href = "https://www.instagram.com/davecummings/?hl=en"
+}
